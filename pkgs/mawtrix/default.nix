9e1658bcd258488ac95c9383c702ed3401778bd2
GoblinKing <goblinking_f@proton.me>
2026-05-10 17:25:43 +0200

    mawtrix: init at 0.6
    - Add Mawtrix, a simple Matrix TUI client written in C# using .NET 8.
    
    - Supports homeserver login, DMs, joining public rooms, and inline
    image display in compatible terminals.

---
+++ pkgs/mawtrix/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildDotnetModule
+, fetchFromGitHub
+, dotnet-sdk_8
+, dotnet-runtime_8
+, openssl
+}:
+
+buildDotnetModule rec {
+  pname = "mawtrix";
+  version = "0.6";
+
+  src = fetchFromGitHub {
+    owner = "Entarno54";
+    repo = "Mawtrix";
+    rev = "8353ceb20168405259eb56402c2978290af07857";
+    hash = "sha256-mMeWXojnCzCM9iM4Zfi1TR3KheTHpB7VX64McRNqgmU=";
+  };
+
+  nugetDeps = src + "/deps.json";
+
+  projectFile = "Mawtrix/Mawtrix.csproj";
+  executables = [ "Mawtrix" ];
+
+  dotnet-sdk = dotnet-sdk_8;
+  dotnet-runtime = dotnet-runtime_8;
+
+  buildInputs = [ openssl ];
+
+  meta = with lib; {
+    description = "A simple Matrix TUI client written in C#";
+    homepage = "https://github.com/Entarno54/Mawtrix";
+    changelog = "https://github.com/Entarno54/Mawtrix/commits/main";
+    license = licenses.mit;
+    maintainers = with maintainers; [ goblinkingdev ];
+    mainProgram = "Mawtrix";
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
